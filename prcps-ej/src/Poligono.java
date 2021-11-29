5ea143af72e6d2f57e1958dcb62ec8cd330f21ff
IvanDario141 <73681727+IvanDario141@users.noreply.github.com>
2021-11-28 21:20:02 -0500

    Delete prcps-ej directory

--- prcps-ej/src/Poligono.java
+++
@@ -1,8 +0,0 @@
-public class Poligono extends Figura{
-    
-    public Poligono(double array[]) {
-        super(4);
-        lados = array;
-    }
-    
-}
--- prcps-ej/src/Triangulo.java
+++
@@ -1,9 +0,0 @@
-public class Triangulo extends Figura{
-    
-    
-    public Triangulo(double array[]) {
-        super(3);
-        lados = new double[3];
-        lados = array;
-    }
-}
